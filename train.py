c090dbe7f853a48ad9bd3f12da18592c66948ee3
icoxfog417 <icoxfog417@yahoo.co.jp>
2017-06-09 11:30:53 +0900

    fix typo

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def flatten(data):
     return np.array(flatted)
 
 
-def train_baseline(network_size, dataset_kind):
+def train_baseline(network_size, dataset_kind, epochs=40):
     # prepare the data
     dp = DataProcessor()
     ptb = dp.get_ptb(DATA_ROOT, vocab_size=10000, force=True)
@@ -29,11 +29,11 @@ def train_baseline(network_size, dataset_kind):
     # make one hot model
     model = OneHotModel(vocab_size, sentence_size, network_size, dataset_kind)
     model.compile()
-    model.fit(x_valid, y_valid, x_valid, y_valid, epochs=20)
+    model.fit(x_valid, y_valid, x_valid, y_valid, epochs=epochs)
     model.save(MODEL_ROOT)
 
 
-def train_augmented(network_size, dataset_kind, tying=False):
+def train_augmented(network_size, dataset_kind, tying=False, epochs=40):
     # prepare the data
     dp = DataProcessor()
     ptb = dp.get_ptb(DATA_ROOT, vocab_size=10000)
@@ -45,7 +45,7 @@ def train_augmented(network_size, dataset_kind, tying=False):
     # make one hot model
     model = AugmentedModel(vocab_size, sentence_size, network_size, dataset_kind, tying=tying)
     model.compile()
-    model.fit(x_valid, y_valid, x_valid, y_valid, epochs=20)
+    model.fit(x_valid, y_valid, x_valid, y_valid, epochs=epochs)
     model.save(MODEL_ROOT)
 
 
@@ -57,12 +57,13 @@ if __name__ == "__main__":
                         help="use tying model")
     parser.add_argument("--nsize", default="small", help="network size (small, medium, large)")
     parser.add_argument("--dataset", default="ptb", help="dataset kind (ptb or wiki2)")
+    parser.add_argument("--epochs", type=int, default=40, help="epoch to train")
     args = parser.parse_args()
 
     n_size = args.nsize
     dataset = args.dataset
     if args.aug or args.tying:
         print("Use Augmented Model (tying={})".format(args.tying))
-        train_augmented(n_size, dataset, args.tying)
+        train_augmented(n_size, dataset, args.tying, args.epochs)
     else:
-        train_baseline(n_size, dataset)
+        train_baseline(n_size, dataset, args.epochs)
